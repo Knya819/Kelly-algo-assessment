6ecb7647e6154b532a50d4d5388e8ece483ccaba
KellyNya <knya819@gmail.com>
2024-09-12 10:03:13 +0100

    general work

--- algo-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/Draft.java
+++ algo-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/Draft.java
@@ -1,52 +1,55 @@
 package codingblackfemales.gettingstarted;
 
 import codingblackfemales.action.Action;
+import codingblackfemales.action.NoAction;
 import codingblackfemales.action.CreateChildOrder;
+import codingblackfemales.algo.AlgoLogic;
 import codingblackfemales.sotw.SimpleAlgoState;
-import codingblackfemales.util.Util;
 import messages.order.Side;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class MySimpleAlgoLogic implements AlgoLogic {
+public class Draft implements AlgoLogic {
 
-    private static final Logger logger = LoggerFactory.getLogger(MySimpleAlgoLogic.class);
+    private static final Logger logger = LoggerFactory.getLogger(Draft.class);
 
     private long buyPrice;
     private long sellPrice;
-    private long quantity = 100;
     private boolean hasBought = false;
-
+    private long Quantity = 10;  
     /**
-     * This simple Algo waits for the stock to drop below a certain price, 100, to buy,
-     * and once the stock price rises 10 units above the buy price, it sells.
+     * For this first attempt i just want to buy and sell in order to have a profit. 
+     * Once this code is giving me the result I want I'll extend it. Like finding the price, getting the quantity or else.
      */
+
     @Override
     public Action evaluate(SimpleAlgoState state) {
 
-        // Get the current price from the order book (best bid)
-        var currentPrice = state.getBidAt(0).price;
+        // Get the current bid and ask prices from the order book (this can be find in AbstractAlgoTest)
+        var currentBidPrice = state.getBidAt(0).price;
+        var currentAskPrice = state.getAskAt(0).price;
 
-        logger.info("[MYALGO] Current price is: " + currentPrice);
+        logger.info("[MYALGO] Current bid price is: " + currentBidPrice);
+        logger.info("[MYALGO] Current ask price is: " + currentAskPrice);
 
-        // Buy Logic: Buy if we haven't bought yet and the price is low
-        if (!hasBought && currentPrice < 100) {
-            buyPrice = currentPrice;
+        // Buying Logic: Buy if we haven't bought yet and the bid price is below 100
+        if (!hasBought && currentBidPrice <= 100) {
+            buyPrice = currentBidPrice;
             hasBought = true;
-            logger.info("[MYALGO] Buying " + quantity + " at price " + buyPrice);
-            return new CreateChildOrder(Side.BUY, quantity, buyPrice);
+            logger.info("[MYALGO] Buying at price " + buyPrice);
+            return new CreateChildOrder(Side.BUY, Quantity, buyPrice);  
         }
 
-        // Sell Logic: Sell if the price goes higher than our buy price + 10
-        if (hasBought && currentPrice > buyPrice + 10) {
-            sellPrice = currentPrice;
+        // Selling Logic: Sell if the ask price is higher than or equal to our buy price + 10, i want to have a proffit of at least 10
+        if (hasBought && currentAskPrice >= buyPrice + 10) {
+            sellPrice = currentAskPrice;
             hasBought = false;
-            logger.info("[MYALGO] Selling " + quantity + " at price " + sellPrice);
-            return new CreateChildOrder(Side.SELL, quantity, sellPrice);
+            logger.info("[MYALGO] Selling at price " + sellPrice);
+            return new CreateChildOrder(Side.SELL, Quantity, sellPrice);  
         }
 
-        // Hold: No action if we haven't met the buying or selling conditions
-        logger.info("[MYALGO] Holding position. Current price: " + currentPrice);
-        return Action.noAction();
+        // No action if we haven't met the buying or selling conditions (just wait)
+        logger.info("[MYALGO] Holding position. Current bid price: " + currentBidPrice + ", ask price: " + currentAskPrice);
+        return NoAction.NoAction; 
     }
 }
--- algo-exercise/getting-started/src/test/java/codingblackfemales/gettingstarted/DraftTest.java
+++ algo-exercise/getting-started/src/test/java/codingblackfemales/gettingstarted/DraftTest.java
@@ -1,35 +1,24 @@
 package codingblackfemales.gettingstarted;
 
 import codingblackfemales.algo.AlgoLogic;
+import codingblackfemales.gettingstarted.Draft;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals; // Importing the correct assertion method
 
-
-/**
- * This test is designed to check your algo behavior in isolation of the order book.
- *
- * You can tick in market data messages by creating new versions of createTick() (ex. createTick2, createTickMore etc..)
- *
- * You should then add behaviour to your algo to respond to that market data by creating or cancelling child orders.
- *
- * When you are comfortable you algo does what you expect, then you can move on to creating the MyAlgoBackTest.
- *
- */
-public class MyAlgoTest extends AbstractAlgoTest {
+public class DraftTest extends AbstractAlgoTest {
 
     @Override
     public AlgoLogic createAlgoLogic() {
-        //this adds your algo logic to the container classes
+        // This adds your Draft algo logic to the container classes
         return new Draft();
     }
 
-
     @Test
     public void testDispatchThroughSequencer() throws Exception {
-
-        //create a sample market data tick....
+        // Create a sample market data tick
         send(createTick());
 
-        //simple assert to check we had 3 orders created
-        //assertEquals(container.getState().getChildOrders().size(), 3);
+        // Simple assert to check if child orders are created or canceled as expected
+        assertEquals(1, container.getState().getChildOrders().size());
     }
 }
