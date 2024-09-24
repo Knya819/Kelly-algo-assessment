22f6ed7ae92f9698522a0e79a3ff36e36ce37c07
KellyNya <knya819@gmail.com>
2024-09-24 14:14:05 +0100

    Added OrderManager,java

---
+++ algo-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/helpers/OrderManager.java
@@ -0,0 +1,22 @@
+package codingblackfemales.gettingstarted.helpers;
+
+import codingblackfemales.action.Action;
+import codingblackfemales.action.CancelChildOrder;
+import codingblackfemales.sotw.ChildOrder;
+import codingblackfemales.sotw.SimpleAlgoState;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OrderManager {
+
+    private static final Logger logger = LoggerFactory.getLogger(OrderManager.class);
+
+    public static Action cancelOldestOrder(SimpleAlgoState state) {
+        if (state.getActiveChildOrders().size() >= 3) {
+            ChildOrder orderToCancel = state.getActiveChildOrders().get(0);  // Cancel the first active order: Not sure if is better to cancel  the first or the less profitable one
+            logger.info("[OrderManager] Cancelling order: " + orderToCancel);
+            return new CancelChildOrder(orderToCancel);
+        }
+        return null;
+    }
+}
