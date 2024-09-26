e5a3c7019854f44ffca35cfffdd54342dddde199
KellyNya <knya819@gmail.com>
2024-09-26 19:12:30 +0100

    Updated test fille, filled quantity not giving the right answer

--- algo-exercise/getting-started/src/test/java/codingblackfemales/gettingstarted/MyAlgoTest.java
+++ algo-exercise/getting-started/src/test/java/codingblackfemales/gettingstarted/MyAlgoTest.java
@@ -1,6 +1,10 @@
 package codingblackfemales.gettingstarted;
 
 import codingblackfemales.algo.AlgoLogic;
+import codingblackfemales.sotw.ChildOrder;
+import codingblackfemales.gettingstarted.helpers.OrderHelper;  
+
+
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -20,8 +24,20 @@ public class MyAlgoTest extends AbstractAlgoTest {
         // Simulate market data by sending a tick through the system
         send(createTick());
 
-        //Verify that 3 child orders were created in total
+        // Verify that 3 child orders were created in total
         assertEquals(3, container.getState().getChildOrders().size());
 
+        // Simulate additional market data moving towards us
+        send(createTick());
+
+        // Then: get the state
+        var state = container.getState();
+
+        // Calculate the total filled quantity across all child orders
+         long filledQuantity = OrderHelper.calculateFilledQuantity(state);
+
+        // Check that our algo state was updated to reflect fills
+        assertEquals(0, filledQuantity);
     }
 }
+
