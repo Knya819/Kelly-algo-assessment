1d992cccb850397f581c1bf0fbb15bbd0354235c
KellyNya <knya819@gmail.com>
2024-10-01 07:17:07 +0100

    Changed interface by taking just state as param

--- algo-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/strategies/ExecutionStrategy.java
+++ algo-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/strategies/ExecutionStrategy.java
@@ -1,12 +1,8 @@
 package codingblackfemales.gettingstarted.strategies;
 
 import codingblackfemales.action.Action;
-import codingblackfemales.sotw.marketdata.AskLevel;
-import codingblackfemales.sotw.marketdata.BidLevel;
 import codingblackfemales.sotw.SimpleAlgoState;
 
-import java.util.List;
-
 public interface ExecutionStrategy {
-    Action execute(SimpleAlgoState state, List<BidLevel> localBidLevels, List<AskLevel> localAskLevels);
+    Action execute(SimpleAlgoState state);
 }
